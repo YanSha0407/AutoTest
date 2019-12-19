44df73be38834304cae7fc0b5a265797e900f00e
yansha <1224894560@qq.com>
2019-12-19 14:22:07 +0800

    练习BeforeSuite beforeClass BeforeMethod AfterMethod AfterClass AfterSuite 执行时间及顺序

--- Chapter5/src/test/java/com/course/testNG/BasicAnnotation.java
+++ Chapter5/src/test/java/com/course/testNG/BasicAnnotation.java
@@ -4,12 +4,17 @@ import org.testng.annotations.*;
 //import org.junit.Test;
 
 public class BasicAnnotation {
+    // Suite 中可以包含多个Class 也就是可以包含 多个类
+    @BeforeSuite  // 测试套件
+    public void beforeSuite(){
+        System.out.println("beforeSuite");
+    }
     @BeforeClass
     public static void beforeClass(){
         System.out.println("这是在类执行之前打印的");
     }
     @BeforeMethod
-    public void boforeMethod(){
+    public void beforeMethod(){
         System.out.println("这是在测试方法之前打印的");
     }
     @Test
@@ -27,5 +32,9 @@ public class BasicAnnotation {
     @AfterClass
     public static void afterClass(){
         System.out.println("这是在类执行之后打印的");
+    }
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("AfterSuite");
     }
 }
