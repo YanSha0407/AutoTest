57d8ace2c53ec0d946472ec5e0018848ae494770
yansha <1224894560@qq.com>
2019-12-19 16:33:34 +0800

    练习TestNG通过xml文件进行参数化 @Parameter 和xml中parameter标签的使用

---
+++ Chapter5/src/test/java/com/course/Paramter/ParamterTest.java
@@ -0,0 +1,17 @@
+package com.course.Paramter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class ParamterTest {
+    // 通过xml文件进行参数化
+    // 需要注册参数的的key也就是@Parameters({"name1","age1"}) 必须和xml文件中的 保持一致
+    // <parameter name="name1" value="张三"/>
+    // <parameter name="age1" value="18"/>
+    @Test
+    @Parameters({"name1","age1"})
+    public void testParam(String name,int age){
+        System.out.println("name = " + name + ";    age=" + age);
+    }
+}
